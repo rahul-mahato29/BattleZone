bb004d9cba2503f5924e45522995c0d6912f1519
Rahul Mahato <mahatorahul2901@gmail.com>
2025-09-22 23:22:56 +0530

    Initial Commit

---
+++ src/main/java/com/eSports/BattleZone/BattleZoneApplication.java
@@ -0,0 +1,13 @@
+package com.eSports.BattleZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleZoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eSports/BattleZone/BattleZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eSports.BattleZone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
